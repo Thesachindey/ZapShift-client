d1aeddb137f5b4ade5f697aaca9e1eae5bc7387f
Sachin <sachinscode24@gmail.com>
2025-11-27 00:09:07 +0600

    added register page and validation

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -4,16 +4,18 @@ import { Outlet } from 'react-router';
 import authImg from '../assets/authImage.png'
 const AuthLayout = () => {
     return (
-        <div className=' max-w-7xl mx-auto px-6 min-h-screen bg-white '>
-            <div className="py-4 ">
-            <Logo />
-            </div>
-            <div className="flex">
-                <div className="flex-1">
-                    <Outlet></Outlet>
+        <div className="p-5 ">
+            <div className=' max-w-7xl mx-auto px-6 min-h-screen bg-white rounded-2xl'>
+                <div className="py-4 ">
+                    <Logo />
                 </div>
-                <div className="flex-1">
-                    <img src={authImg} alt="" />
+                <div className="flex">
+                    <div className="flex-1">
+                        <Outlet></Outlet>
+                    </div>
+                    <div className="flex-1">
+                        <img src={authImg} alt="" />
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Register = () => {
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
+
+    const handelRegistration = (data) => {
+        console.log(data)
+    }
+
     return (
-        <div>
-            this is register page
+        <div className='flex justify-center items-center h-full'>
+            <form onSubmit={handleSubmit(handelRegistration)}>
+                <fieldset className="fieldset  rounded-box w-xs border p-4">
+                    {/* <legend className="fieldset-legend">Login</legend> */}
+                    {/* email  */}
+                    <label className="label">Email</label>
+                    <input type="email" {...register("email", { required: true })} className="input" placeholder="Email" />
+                    {/* email validation  */}
+                    {
+                        errors.email?.type === 'required' && <p className='text-red-500'>Email is required.</p>
+                    }
+
+                    {/* password */}
+                    <label className="label">Password</label>
+                    <input type="password" {...register("password", {
+                        required: true,
+                        minLength: 6,
+                        pattern: /^(?=\S*$)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{6,}$/
+
+
+                    })} className="input" placeholder="Password" />
+                    {
+                        errors.password?.type === 'required' && <p className='text-red-500'>Password is required.</p>
+                    }
+                    {
+                        errors.password?.type === 'minLength' && <p className='text-red-500'>Min length should be 6.</p>
+                    }
+                    {
+                        errors.password?.type === 'pattern' && <p className='text-red-500'>Password must include Uppercase + Lowercase + Number + Special Character + No Spaces and minimum 6 characters.</p>
+                    }
+                    <button className="btn btn-neutral mt-4">Register</button>
+                </fieldset>
+            </form>
         </div>
     );
 };

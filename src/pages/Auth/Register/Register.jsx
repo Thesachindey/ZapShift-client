e068e9d91fdc61930418d164941a1300841c0ed2
Sachin <sachinscode24@gmail.com>
2025-11-28 11:58:27 +0600

    added api keys

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -25,40 +25,6 @@ const Register = () => {
     }
 
     return (
-        //    { // <div className='flex justify-center items-center h-full'>
-        //     //     <form onSubmit={handleSubmit(handelRegistration)}>
-        //     //         <fieldset className="fieldset  rounded-box w-xs border p-4">
-        //     //             {/* <legend className="fieldset-legend">Login</legend> */}
-        //     //             {/* email  */}
-        //     //             <label className="label">Email</label>
-        //     //             <input type="email" {...register("email", { required: true })} className="input" placeholder="Email" />
-        //     //             {/* email validation  */}
-        //     //             {
-        //     //                 errors.email?.type === 'required' && <p className='text-red-500'>Email is required.</p>
-        //     //             }
-
-        //     //             {/* password */}
-        //     //             <label className="label">Password</label>
-        //     //             <input type="password" {...register("password", {
-        //     //                 required: true,
-        //     //                 minLength: 6,
-        //     //                 pattern: /^(?=\S*$)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{6,}$/
-
-
-        //     //             })} className="input" placeholder="Password" />
-        //     //             {
-        //     //                 errors.password?.type === 'required' && <p className='text-red-500'>Password is required.</p>
-        //     //             }
-        //     //             {
-        //     //                 errors.password?.type === 'minLength' && <p className='text-red-500'>Min length should be 6.</p>
-        //     //             }
-        //     //             {
-        //     //                 errors.password?.type === 'pattern' && <p className='text-red-500'>Password must include Uppercase + Lowercase + Number + Special Character + No Spaces and minimum 6 characters.</p>
-        //     //             }
-        //     //             <button className="btn btn-neutral mt-4">Register</button>
-        //     //         </fieldset>
-        //     //     </form>
-        //     // </div>}
 
         <div className='flex flex-col justify-center items-center  h-full pb-10'>
             <div className="flex flex-col items-center justify-center py-4">
@@ -68,6 +34,32 @@ const Register = () => {
             <form onSubmit={handleSubmit(handelRegistration)}>
                 <fieldset className="fieldset w-xs  p-4">
                     {/* <legend className="fieldset-legend">Login</legend> */}
+
+                    {/* name  */}
+                    <label className="label">Name</label>
+                    <input {...register("name", { required: true, minLength: 4 })} type="text" className="input" placeholder="Your name" />
+                    {/* name validation  */}
+                    {errors.name?.type === "required" && (
+                        <p role="alert" className='text-red-500'>Name is required.</p>
+                    )}
+                    {errors.name?.type === "minLength" && (
+                        <p role="alert" className='text-red-500'>Name must be at least 4 characters.</p>
+                    )}
+
+                    {/* img  */}
+
+                    <label className="label">Image</label>
+                    <input
+                        {...register("image", { required: true })}
+                        type="file"
+                        
+                        className="file-input file:bg-primary file:text-white file:border-0"
+                    />
+
+                    {/* email validation  */}
+                    {errors.image?.type === "required" && (
+                        <p role="alert" className='text-red-500'>Image is required.</p>
+                    )}
                     {/* email  */}
                     <label className="label">Email</label>
                     <input {...register("email", { required: true })} type="email" className="input" placeholder="Email" />
@@ -75,6 +67,7 @@ const Register = () => {
                     {errors.email?.type === "required" && (
                         <p role="alert" className='text-red-500'>Email is required.</p>
                     )}
+
                     {/* password  */}
                     <label className="label">Password</label>
                     <input {...register("password", {
